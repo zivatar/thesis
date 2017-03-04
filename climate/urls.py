38cfd0e5c9ee49a6e1776e6f7cb381cd39dba3e5
zivatar <macgyver1024@gmail.com>
2017-03-04 21:06:31 +0100

    [m] Modify and save sites

--- climate/urls.py
+++ climate/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
 	url(r'^site_list/', views.site_list, name='site_list'),
 	url(r'^new_site/', views.new_site, name='new_site'),
 	#url(r'^new_sites/', views.new_sites, name='new_sites'),
-	url(r'^site/(?P<pk>[0-9]+)/$', views.site_details, name='site_details'),
+	url(r'^site/(?P<pk>[0-9]+)/details$', views.site_details, name='site_details'),
+	url(r'^site/(?P<pk>[0-9]+)/edit$', views.site_edit, name='site_edit'),
 ]
--- climate/views.py
+++ climate/views.py
@@ -17,7 +17,6 @@ def site_list(request):
 @login_required
 def new_site(request):
 	if request.method == "POST":
-		print("POST++++")
 		form = SiteForm(request.POST)
 		if form.is_valid():
 			site = form.save(commit=False)
@@ -36,3 +35,17 @@ def main(request):
 def site_details(request, pk):
 	site = get_object_or_404(Site, pk=pk)
 	return render(request, 'climate/site_details.html', {'site' : site})
+
+def site_edit(request, pk):
+	site = get_object_or_404(Site, pk=pk)
+	if request.method == "POST":
+		form = SiteForm(request.POST, instance=site)
+		if form.is_valid():
+			site = form.save(commit=False)
+			#site.owner = request.user
+			site.save()
+			return redirect(site_edit, pk=site.pk)
+	else:
+		form = SiteForm()
+		sites = Site.objects.filter(owner=request.user)
+	return render(request, 'climate/site_edit.html', {'sites': sites, 'wide_area': WIDE_AREA, 'narrow_area': NARROW_AREA, 'form': form, 'site': site})
