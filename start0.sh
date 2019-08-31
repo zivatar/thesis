a4770af92a55efea5d7e247ed859c0f3dc137970
MacGyver <macgyver1024@gmail.com>
2019-08-31 12:40:28 +0200

    Docker - using nginx & gunicorn works

--- start0.sh
+++ start0.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-sleep 10
-python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:8000
+#sleep 10
+#python3 manage.py migrate
+#python3 manage.py runserver 0.0.0.0:8000
+gunicorn --timeout=30 --workers=2 --bind 0.0.0.0:8000 django_app.wsgi:application
